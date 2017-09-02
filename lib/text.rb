1bb4e59056a149957fc4f51f0ed24f142404ae65
Jessica Burns <jburns@neboagency.com>
2017-09-02 15:47:43 -0400

    Redo game text

--- lib/text.rb
+++ lib/text.rb
@@ -1,4 +1,7 @@
 class Text
+  require 'colorize'
+
+  # COLOR OPTIONS
   #:black   => 0, :light_black    => 60,
   #:red     => 1, :light_red      => 61,
   #:green   => 2, :light_green    => 62,
@@ -8,32 +11,31 @@ class Text
   #:cyan    => 6, :light_cyan     => 66,
   #:white   => 7, :light_white    => 67,
   #:default => 9
-  require 'colorize'
 
   ###################
   ### Game Globals ###
   ##############################################
-  def self.discorama
-    "DISC".magenta+ "-O-".green + "RAMA".magenta
+  PLAYER_NAME = "DANCEBOT 9000".green
+  DISCORAMA = "DISC".magenta+ "-O-".green + "RAMA".magenta
+  PROMPT = "ENTER COMMAND: ".green
+  ERROR = "ERROR: COMMAND NOT FOUND IN EXECUTABLE DANCEBOT_9000.vb".red
+  EXIT = "HALTING DANCEBOT_9000 PROCESS. HASTA LA VISTA.".green
+  MAP_ERROR = "ERROR: PATH UNAVAILABLE".red
+
+  def self.error
+    puts ERROR
   end
 
-  def self.discorama_sign
-    puts "     ******************************"
-    puts "     * " + ("Welcome to the ".green + self.discorama).blink + " *"
-    puts "     ******************************"
-    puts "                   ||"
-    puts "                   ||"
-    puts "                   ||"
-    puts "                   ||"
-    puts "                   ||"
-  end
-
-  def self.disco_royalty
-    "disco royalty".blue
+  def self.exit
+    puts EXIT
   end
 
   def self.prompt
-    print "What's the plan, man? ".light_blue
+    print PROMPT
+  end
+
+  def self.map_barrier
+    puts MAP_ERROR
   end
 
   ###################
@@ -41,63 +43,28 @@ class Text
   ##############################################
   def self.intro
     system "clear"
-    self.discorama_sign
-    puts "Groovy, baby! You've stepped out into the swingin' city nightlife tonight to prove you are " + Text::disco_royalty + "!"
-    puts "You walk into the most happenin' club around, the disco dance talk of the town. A large neon sign guides the way."
-  end
-
-  #################
-  ### Game Exit ###
-  ##############################################
-  def self.exit
-    "Catch ya on the flip side, sweet-cheeks!"
+    puts "INITIATING DANCEBOT_9000 PROCESS".green
+    puts "The year is 3017."
+    puts "Robots have become a part of everyday life and can be found in the workforce and at home."
+    puts "However, there's one place that robots haven't been able #{"DOMINATE".yellow} just yet: the dancefloor."
+    puts "You are #{PLAYER_NAME} and you are ready to usher in a new era of #{"SUPERIOR DANCING ABILITY".yellow}."
+    puts "You've come to the #{"GROOVIEST".yellow} club in the world to battle the greatest #{"HUMAN".yellow} dancers of this generation."
+    puts "By the end of the night you hope to show the #{"HUMANS".yellow} your #{"DANCING SUPREMACY".yellow.blink}"
+    puts "A neon sign guides the way..."
   end
 
   ###################
   ### Help ###
   ##############################################
   def self.help
-    "\n" +
-    "Hey there baby-cakes! The name of this dicso game to to become #{disco_royalty}!" + "\n" +
-    "How do you do that, you ask? You have to dance battle your way to the top!" + "\n" +
-    "You can move around the club by using the commands: " + "north, east, south, or west".red + "\n" +
-    "You can use your eye-beams to assess the situation with the command " + "look".red + "\n" +
-    "You can talk to your fellow groovy club goers by using the command " + "talk".red + "\n" +
-    "You can initiate a dance battle with the command " + "dance".red + "\n" +
-    "You can pick up items with the command " + "take".red + "\n" +
-    "If you need to peace-out early (lame-o), you can use the command " + "exit".red + "\n"
+    puts ""
+    puts "GREETINGS #{PLAYER_NAME}"
+    puts "OBJECTIVE: Battle #{"HUMANS".yellow} to become the #{"GREATEST DANCER ON EARTH".yellow}"
+    puts "NAVIGATION: Use #{"north, east, south, or west".blue} to navigate the #{DISCORAMA}"
+    puts "OBSERVATION: Use #{"look".blue} to view the #{DISCORAMA}"
+    puts "ENGAGEMENT: Use #{"talk".blue} to converse with #{"HUMANS".yellow} and #{"take".blue} to pickup objects"
+    puts "TERMINATION: Use #{"exit".blue} to abort the mission early."
   end
 
   #########################
-  ### Room Intros + Map ###
-  ##############################################
-  def self.bar
-    "You are at the #{Text::discorama} bar. The grooviest bar in town.\n...at least, that's what the souvenir cups say."
-  end
-
-  def self.dancefloor
-    "You are at the dancefloor of the #{Text::discorama}"
-  end
-
-  def self.entrance
-    "You are in the entrance of the #{Text::discorama}\nThere are several cool cats hangin' out here."
-  end
-
-  def self.lounge
-    "You are in the lounge of the #{Text::discorama}"
-  end
-
-  def self.mensbathroom
-    "You are in the men's bathroom of the #{Text::discorama}\nInexplicably, smooth jazz is playing over the speakers."
-  end
-
-  def self.womensbathroom
-    "You are in the women's bathroom of the #{Text::discorama}\nIt's alarmingly dark in here."
-  end
-
-  def self.map_barrier
-    "Uh-oh daddy-o! There's no more club over there! Time to turn this party around.".red
-  end
-  ##############################################
-
 end
