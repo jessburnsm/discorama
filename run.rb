6763ce625d1e7bd94e829ccf321fc72b7559255a
Jessica Burns <jburns@neboagency.com>
2017-09-02 15:48:13 -0400

    Update run logic

--- run.rb
+++ run.rb
@@ -1,53 +1,61 @@
 Dir["lib/**.*", "lib/*/**.*"].each { |file| require_relative file }
-require 'colorize'
 
 class Game
-  ACTIONS = [
-    :north, :east, :south, :west, :look, :fight, :take, :status, :help, :exit
-  ]
-
   def initialize
     @world = World.new
     @player = Player.new
-
     start_game
   end
 
   private
+
   def start_game
-    Text::intro
+    begin
+      Text::intro
 
-    while @player.alive?
-      @current_room = @world.get_room_of(@player)
-
-      print_status
-
-      action = take_player_input
-      next unless ACTIONS.include? action
-
-      take_action(action)
+      while @player.alive?
+        @current_room = @world.get_room_of(@player)
+        print_status
+        take_action(parse_player_input)
+      end
+    rescue SystemExit, Interrupt # Catpure ctrl+c exit, end gracefully
+      puts ""
+      Text::exit
+      exit
     end
   end
 
-  def take_player_input
+  def parse_player_input
+    articles = ['a ', 'an ', 'the ', 'to ', 'go ', 'at ']
+    regex = Regexp.union(articles)
+
     Text::prompt
-    gets.chomp.to_sym
+    parsed = gets.chomp.downcase.gsub(regex, '').split
+
+    { command: (parsed[0].to_sym unless parsed[0].nil?), target: ( parsed[1].to_sym unless parsed[1].nil? ) }
   end
 
   def print_status
     puts ""
-    # puts "You are at map coordinates [#{@player.x_coord}, #{@player.y_coord}]"
-
     puts @current_room
-    if @current_room.content
-      puts "You see #{@current_room.content}."
+    @current_room.content.each do |key, content|
+      puts content
+    end
+    # puts "You are at map coordinates [#{@player.x_coord}, #{@player.y_coord}]"
+  end
+
+  def execute_interactive_action(command, target)
+    if target && @current_room.content.key?(target)
+      @current_room.content[target].send(command)
+    else
+      Text::error
     end
   end
 
   def take_action(action)
-    case action
-    when :look
-      print_status
+    case action[:command]
+    when :look, :talk, :take
+      execute_interactive_action(action[:command], action[:target])
     when :north
       @world.move_entity_north(@player)
     when :east
@@ -56,15 +64,18 @@ class Game
       @world.move_entity_south(@player)
     when :west
       @world.move_entity_west(@player)
-    when :fight, :take
-      @current_room.interact(@player)
-    when :status
-      @player.print_status
+    # when :fight, :take
+    #   @current_room.interact(@player)
+    # when :status
+    #   @player.print_status
     when :help
-      puts Text::help
+      Text::help
     when :exit
-      puts Text::exit
+      Text::exit
       exit
+    when nil
+    else
+      Text::error
     end
   end
 end
