5b40e0104129a0e95b1fd423a782918ebdc417be
Jessica Burns <jburns@neboagency.com>
2017-09-06 13:52:28 -0400

    Remove old code from action director
    Behavior has been extracted out to command classes

--- lib/utilities/action_director.rb
+++ lib/utilities/action_director.rb
@@ -15,31 +15,5 @@ class ActionDirector
     rescue TargetNotFound
       @game_text.target_error
     end
-
-    # puts ""
-    # case action[:command]
-    # when :look, :talk, :take
-    #   execute(action[:command], action[:target])
-    # when :north
-    #   @world.move_entity_north(@player)
-    #   #@player.current_room = @world.get_room_from(@player.current_room, y: -1)
-    # when :east
-    #   @world.move_entity_east(@player)
-    # when :south
-    #   @world.move_entity_south(@player)
-    # when :west
-    #   @world.move_entity_west(@player)
-    # # when :fight, :take
-    # #   @current_room.interact(@player)
-    # # when :status
-    # #   @player.print_status
-    # when :help
-    #   @game_text.help
-    # when :exit
-    #   exit
-    # when nil
-    # else
-    #   @game_text.error
-    # end
   end
 end
