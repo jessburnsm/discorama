c878a8ea5c187d78b10d93936071b81564cda5ab
Jessica Burns <jburns@neboagency.com>
2017-09-06 13:47:41 -0400

    Implement addititional game commands
    Adds help and movement commands
    Also rearranges file for readability

--- lib/commands/command.rb
+++ lib/commands/command.rb
@@ -1,20 +1,3 @@
-# Responsible for instantiating correct command class
-# based on received user input
-class Command
-  def self.get_command(user_input)
-    case user_input
-    when :look
-      Command::Look.new
-    when :talk
-      Command::Talk.new
-    when :exit
-      Command::Exit.new
-    else
-      raise CommandNotFound, user_input
-    end
-  end
-end
-
 # Generic error for when a command is not found
 # in our system
 class CommandNotFound < StandardError
@@ -25,10 +8,30 @@ end
 class TargetNotFound < StandardError
 end
 
+# Responsible for instantiating correct command class
+# based on received user input
+class Command
+  def self.get_command(user_input)
+    case user_input
+    when :north, :east, :south, :west
+      Command::Movement.new(user_input)
+    when :look
+      Command::Look.new
+    when :talk
+      Command::Talk.new
+    when :help
+      Command::Help.new
+    when :exit
+      Command::Exit.new
+    else
+      raise CommandNotFound, user_input
+    end
+  end
+end
 
 # Responsible for execution of look command
 class Command::Look
-  def execute(target, current_room)
+  def execute(target, current_room, *)
     if target && current_room.has_target?(target)
       current_room.get_target(target).look
     elsif target
@@ -41,7 +44,7 @@ end
 
 # Responsible for execution of talk command
 class Command::Talk
-  def execute(target, current_room)
+  def execute(target, current_room, *)
     if target && current_room.has_target?(target)
       current_room.get_target(target).talk
     elsif target
@@ -52,9 +55,91 @@ class Command::Talk
   end
 end
 
+# Responsible for execution of help command
+class Command::Help
+  def execute(*)
+    GameText.new.help
+  end
+end
+
 # Responsible for execution of exit command
 class Command::Exit
   def execute(*)
     exit
   end
 end
+
+class Command::Movement
+  # MOVE_MAPPING = {
+  #   north: { y: -1, x: 0 },
+  #   south: { y: 1, x: 0 }
+  # }
+
+  def initialize(direction)
+    @direction = direction
+  end
+
+  def execute(target, current_room, world, player)
+    puts "MOVEMENT COMMAND #{@direction}"
+    case @direction
+    when :north
+      world.move_entity_north(player)
+    when :east
+      world.move_entity_east(player)
+    when :west
+      world.move_entity_west(player)
+    when :south
+      world.move_entity_south(player)
+    end
+  #   direction_changes = MOVE_MAPPING[@direction]
+  #   next_room = world.get_room(player.current_room, direction_changes)
+  #   player.current_room = next_room
+  # rescue InvalidMovement => e
+  #   GameText.invalid_move
+  end
+end
+
+# class Command
+#   def self.get_command(user_input)
+#     case user_input
+#     when :north
+#       Command::Movement.new(:north)
+#     end
+#   end
+# end
+
+# class Command::Movement
+#   MOVE_MAPPING = {
+#     north: { y: -1, x: 0 },
+#     south: { y: 1, x: 0 }
+#   }
+
+#   def initialize(direction)
+#     @direction = direction
+#   end
+
+#   def execute(world, player)
+#     direction_changes = MOVE_MAPPING[@direction]
+#     next_room = world.get_room(player.current_room, direction_changes)
+#     player.current_room = next_room
+#   rescue InvalidMovement => e
+#     GameText.invalid_move
+#   end
+# end
+
+# class Command::Attack
+#   def execute(world, player)
+#   end
+# end
+
+# class Command::Talk
+#   def execute(world, player)
+#   end
+# end
+
+# class Command::Help
+#   def execute(*)
+#   end
+# end
+
+# command.execute(world, player)
