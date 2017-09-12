4e36a86c658f82e6bc73cd1fe00d9b4ad313c343
Jessica Burns <jburns@neboagency.com>
2017-09-12 16:04:20 -0400

    Redo Jerry's dialog

--- lib/people/jerry.rb
+++ lib/people/jerry.rb
@@ -10,8 +10,21 @@ class Jerry < Person
   end
 
   def look
-    puts "How do you know #{@name}'s name is #{@name}? He happens to be wearing a HELLO MY NAME IS '#{@name}' tag. It clashes with his outfit."
-    puts "#{"HUMANS".yellow} are strange, aren't they? Despite his poor fashion sense, he looks like a #{"NON-THREAT".yellow}."
+    puts "How do you know #{@name}'s name is #{@name}? He happens to be wearing a HELLO MY NAME IS '#{@name}' tag."
+    puts "It clashes tragically with his outfit."
+    puts "#{@game_text.emphasis('humans')} are strange, aren't they? Despite his poor fashion sense, he looks like a #{@game_text.emphasis('non-threat')}."
+    puts "You could probably #{@game_text.highlight('talk')} to #{@name}."
+  end
+
+  def talk
+    puts @portrait
+    puts "#{@name}: 'Hey-o, fellow disc-o amig-o! Did you come out for the competition? I've never seen a robot compete before!'"
+    puts "#{@name}: 'I'm real glad you're here though -- it's time to show the #{@game_text.emphasis('elite feet')} some elite defeat!'"
+    puts ""
+    puts "The #{@game_text.emphasis('elite feet')} is a team of the best dancers in the world. They dominate the competition every year."
+    puts ""
+    puts "#{@name}: 'You're looking a little lost though, friend-o. Robots usually come with a #{@game_text.highlight('help')} command, right?'"
+    puts "#{@name}: 'Check back when you've got your bearings!'"
   end
 
   def to_s
