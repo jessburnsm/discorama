1f70e9352724b1bbd77122189b1c01f919c9f895
Jessica Burns <jburns@neboagency.com>
2017-09-12 16:03:59 -0400

    Redo game text, update some methods
    Add methods to specially format text for console output

--- lib/utilities/game_text.rb
+++ lib/utilities/game_text.rb
@@ -16,15 +16,12 @@ class GameText
   ###################
   ### Game Globals ###
   ##############################################
-  PLAYER_NAME = "DANCEBOT 9000".green
-  DISCORAMA = "DISC".magenta+ "-O-".green + "RAMA".magenta
-
   def command_error(command)
-    puts "ERROR: COMMAND '#{command.to_s.upcase}' NOT FOUND IN EXECUTABLE DANCEBOT_9000.vb".red
+    puts "ERROR: COMMAND '#{command.to_s.upcase}' NOT FOUND IN EXECUTABLE DANCEBOT_9000.vb (NoMethodError)".red
   end
 
-  def target_error
-    puts "ERROR: COULD NOT FIND SPECIFIED TARGET".red
+  def target_error(target = '')
+    puts "ERROR: OBJECT '#{target.to_s.upcase}' NOT FOUND (NameError)".red
   end
 
   def exit
@@ -34,10 +31,11 @@ class GameText
 
   def map_barrier
     puts "ERROR: PATH UNAVAILABLE".red
-    puts "Someday robots may be able to pass through walls. Sadly, today is not that day."
+    puts "...Someday robots may be able to pass through walls. Sadly, today is not that day. You'll have to go another way."
   end
 
   def action_prompt
+    puts ""
     print "ENTER COMMAND: ".green
   end
 
@@ -46,11 +44,32 @@ class GameText
   end
 
   def current_status(current_room)
-    puts ""
     puts current_room
     current_room.content.each do |key, content|
       puts content
     end
+  end
+
+  def discorama
+    "The " + "DISC".magenta+ "-O-".green + "RAMA".magenta
+  end
+
+  def player_name
+    "DANCEBOT_9000".green
+  end
+
+  def delimiter
+    puts ""
+    puts "************************************************"
+    puts ""
+  end
+
+  def emphasis(string)
+    string.upcase.yellow
+  end
+
+  def highlight(string)
+    string.blue
   end
 
   ###################
@@ -58,27 +77,52 @@ class GameText
   ##############################################
   def intro
     system "clear"
-    puts "INITIATING DANCEBOT_9000 PROCESS".green
-    puts "The year is 3017."
-    puts "Robots have become a part of everyday life and can be found in the workforce and at home."
-    puts "However, there's one place that robots haven't been able #{"DOMINATE".yellow} just yet: the dancefloor."
-    puts "You are #{PLAYER_NAME} and you are ready to usher in a new era of #{"SUPERIOR DANCING ABILITY".yellow}."
-    puts "You've come to the #{"GROOVIEST".yellow} club in the world to battle the greatest #{"HUMAN".yellow} dancers of this generation."
-    puts "Through your #{"DANCING SUPREMACY".yellow} you hope to inspire young robots everywhere to come on down to #{"BOOGIE TOWN".yellow}."
-    puts "A neon sign guides the way..."
+    puts "INITIATING DANCEBOT_9000 PROCESS... COMPLETE. PRESS ENTER TO BOOT.".green
+    gets
+    puts "The year: 3017."
+    puts "The location: New Atlanta's hottest club, #{discorama}"
+    puts "The mission: Win #{emphasis('THE DISC-O-RAMA DANCE BATTLE 3017 MASTERS CHAMPIONSHIP')} competition."
+    puts ""
+    puts "Ever since the #{emphasis('AI revolution')} of 2050 and the subsequent #{emphasis('robot rights war')} of 2080,"
+    puts "robots have been struggling to define their culture and creative contributions within society."
+    puts ""
+    puts "You are #{player_name}, and your programming has willed you to #{discorama} in New Atlanta to prove that robots can be"
+    puts "as creative as #{emphasis('humans')}. You intend to prove this through #{emphasis('the power of dance')}."
+    puts ""
+    puts "If you can win #{emphasis('THE DISC-O-RAMA DANCE BATTLE 3017 MASTERS CHAMPIONSHIP')} competition tonight, you will advance the culture"
+    puts "of all robots and go down in history as a robotic cultural icon (basically, #{emphasis('robot Elvis')})."
+    puts ""
+    puts "And who doesn't want to be #{emphasis('robot Elvis')}!? A neon sign guides the way..."
     load 'portraits/discorama.rb'
+    puts ""
+    puts "PRESS ENTER TO ENGAGE OPERATION DANCE BATTLE.".green
+    gets
   end
 
   ###################
   ### Help ###
   ##############################################
   def help
-    puts "GREETINGS #{PLAYER_NAME}"
-    puts "OBJECTIVE: Battle #{"HUMANS".yellow} to become the #{"GREATEST DANCER ON EARTH".yellow}"
-    puts "NAVIGATION: Use #{"north, east, south, or west".blue} to navigate the #{DISCORAMA}"
-    puts "OBSERVATION: Use #{"look".blue} to view the #{DISCORAMA}"
-    puts "ENGAGEMENT: Use #{"talk".blue} to converse with #{"HUMANS".yellow} and #{"take".blue} to pickup objects"
-    puts "TERMINATION: Use #{"exit".blue} to abort the mission early."
+    puts "BOOTING DANCEBOT_9000_PLS_HELP.flv".green
+    puts ""
+    puts "--------------------------------------------"
+    puts "| Greetings #{player_name},"
+    puts "|"
+    puts "| I wonder if this documentation finds you well?"
+    puts "| I do hope that you will put forth a great effort to win this year's illustrious"
+    puts "| #{emphasis('DISC-O-RAMA DANCE BATTLE MASTERS CHAMPIONSHIP')}, an honor I could not achieve myself."
+    puts "| (Oh, the ravages of time!)"
+    puts "| "
+    puts "| Please remember that I have refactored your navigation module into four easy commands: #{highlight('north, east, south, west')}."
+    puts "| You may also find #{highlight('look')}, #{highlight('talk')}, and #{highlight('take')} to be brillantly useful for interacting with others."
+    puts "| If you find yourself needing to take a tea break or the like, do use #{highlight('exit')} to 'bow' out of the competition."
+    puts "| (I do amuse myself with such wordplay!)"
+    puts "| "
+    puts "| In good health and happy feet,"
+    puts "| Your friend,"
+    puts "|"
+    puts "| #{emphasis('the professor')}"
+    puts "--------------------------------------------"
   end
 
   #########################
