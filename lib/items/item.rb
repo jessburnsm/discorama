1eb4b4b199c03021e54e6cd576c3eefe2fa414c2
Jessica Burns <jburns@neboagency.com>
2017-09-21 15:20:00 -0400

    Add items to the game

--- lib/items/item.rb
+++ lib/items/item.rb
@@ -1,11 +1,63 @@
 class Item
-  def interact(player)
-    puts "You pick up #{self}"
+  require 'colorize'
 
-    perform_item_effect(player)
+  def initialize(room = nil)
+    @name = set_name
+    @room = room
+    @game_text = GameText.new
+  end
+
+  def key
+    name.uncolorize.downcase.to_sym
+  end
+
+  def set_target_data
+    { key => self }
+  end
+
+  def name
+    @name.to_s.magenta
+  end
+
+  def talk
+    puts "DEFAULT"
+  end
+
+  def take(player)
+    puts "You take the #{name}"
+    @room.remove_content(key)
+    player.add_to_inventory(self)
+  end
+
+  def look
+    puts "DEFAULT"
+  end
+
+  def set_name
+    "DEFAULT"
   end
 
   def to_s
-    "a shiny awesome #{@name}"
+  end
+end
+
+class Umbrella < Item
+  def set_name
+    "umbrella"
+  end
+
+  def look
+    puts "The #{name} is polka-dotted."
+    puts "It appears to have been abandoned, as there hasn't been rain in New Atlanta for days."
+    puts "Looking at the pattern of the #{name} puts you at ease. Odd."
+  end
+
+  def talk
+    puts "You attempt to strike up a conversation with the #{name} but it doesn't seem to be responsive."
+    puts "You take mild offense to this development."
+  end
+
+  def to_s
+    "There is an #{name} propped up against the far wall of the room."
   end
 end
