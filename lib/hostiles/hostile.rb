21e2f8c19ac43a66fe383a52482f7d0e97c1402f
Jessica Burns <jburns@neboagency.com>
2017-10-17 12:00:19 -0400

    Add skills and battle tracker to hostiles

--- lib/hostiles/hostile.rb
+++ lib/hostiles/hostile.rb
@@ -3,11 +3,28 @@ require_relative '../utilities/actor'
 class Hostile < Actor
   MAX_HP = 100
   MAX_CP = 100
+  DANCE_SKILL = 1
+  TAUNT_SKILL = 1
+  RALLY_SKILL = 1
 
   def initialize(room = nil)
     super(room)
     @hp = self.class::MAX_HP
     @cp = self.class::MAX_CP
+    @dance_skill = self.class::DANCE_SKILL
+    @taunt_skill = self.class::TAUNT_SKILL
+    @rally_skill = self.class::RALLY_SKILL
+
+    # Hostile will load actions array according to skills,
+    # favoring their particular strengths
+    @actions = []
+    @dance_skill.times { @actions << :dance }
+    @taunt_skill.times { @actions << :taunt }
+    @rally_skill.times { @actions << :rally }
+
+    # Battle cycle keeps track of how many times the player
+    # has used the battle command on this hostile
+    @battle_cycle = 0
   end
 
   def name
@@ -30,10 +47,11 @@ class Hostile < Actor
 end
 
 class Princess < Hostile
-  attr_accessor :hp, :cp
-
-  MAX_HP = 100
+  MAX_HP = 50
   MAX_CP = 50
+  DANCE_SKILL = 1
+  TAUNT_SKILL = 3
+  RALLY_SKILL = 1
 
   def set_name
     "Princess"
