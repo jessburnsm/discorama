643cfe26c5e1a003cf757b787a56e1fb056229c3
Jessica Burns <jburns@neboagency.com>
2017-10-29 14:22:51 -0400

    Update item dia log output

--- lib/items/item.rb
+++ lib/items/item.rb
@@ -2,6 +2,7 @@ require_relative '../utilities/actor'
 
 class Item < Actor
   def execute_effect(player)
+    @game_text.read_from_yaml(@dialog[:execute], name)
   end
 
   def name
@@ -10,7 +11,6 @@ class Item < Actor
 
   def take(player)
     super
-
     remove_item_from_room(key)
     give_to_player(player, self)
   end
